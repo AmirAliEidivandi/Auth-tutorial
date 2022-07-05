dad1ace46dfd85a4e63328cf71f0c0d5f8101901
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-05 15:08:10 +0430

    handle jwt error

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -7,7 +7,7 @@ module.exports = {
             const payload = {};
             const secret = process.env.ACCESS_TOKEN_SECRET;
             const option = {
-                expiresIn: "1h",
+                expiresIn: "20s",
                 issuer: "pickurpage.com",
                 audience: userId,
             };
@@ -20,6 +20,21 @@ module.exports = {
                 }
                 resolve(token);
             });
+        });
+    },
+    verifyAccessToken: (req, res, next) => {
+        if (!req.headers["authorization"]) return next(createError.Unauthorized());
+        const authHeader = req.headers["authorization"];
+        const bearerToken = authHeader.split(" ");
+        const token = bearerToken[1];
+        JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+            if (err) {
+                const message = err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+                return next(createError.Unauthorized(message));
+            }
+
+            req.payload = payload;
+            next();
         });
     },
 };
